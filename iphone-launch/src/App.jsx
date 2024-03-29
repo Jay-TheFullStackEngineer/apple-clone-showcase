05a38449efa6c1e9378234f90e4a201b0734255f
Jonathan Alex Thompson <147441174+Jay-TheFullStackEnginner@users.noreply.github.com>
2024-03-29 23:56:40 +0200

    3D model & Sentry
    3D model for Iphone, controls on lights, camera and rotation. Size & color toggle.  Sentry setup completed.

--- iphone-launch/src/App.jsx
+++ iphone-launch/src/App.jsx
@@ -1,11 +1,16 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Highlights from "./components/Highlights";
-import Model from "./components/Model";
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import * as Sentry from '@sentry/react'
 
 const App = () => {
+
+  //Sentry test method
+  //return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
-    <main className="bg-black">
+    <main className='bg-black'>
       <Navbar />
       <Hero />
       <Highlights />
@@ -14,4 +19,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- iphone-launch/src/main.jsx
+++ iphone-launch/src/main.jsx
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0fe9df52f691eda5625d43e65e3e14a6@o4506993773182976.ingest.us.sentry.io/4506996364673024",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({useEffect: React.useEffect,}),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
