2f927449533c7de657794206426ff2d9d7e9bac0
Jonathan Alex Thompson <147441174+Jay-TheFullStackEnginner@users.noreply.github.com>
2024-03-30 10:41:10 +0200

    Update Features.jsx
    Added headings, gsap and video.

---
+++ iphone-launch/src/components/Features.jsx
@@ -0,0 +1,43 @@
+import { useGSAP } from '@gsap/react'
+import React,{useRef} from 'react'
+import { animateWithGsap } from '../utils/animations'
+import { exploreVideo } from '../utils'
+
+const Features = () => {
+    useGSAP(() =>{
+        animateWithGsap('#features_title', {y: 0, opacity: 1})
+    },[])
+
+    const videoRef = useRef();
+
+  return (
+    <section className='h-full common-padding bg-zinc relative overflow-hidden'>
+        <div className='screen-max-width'>
+            <div className='mb-12 w-full'>
+                <h1 id='features_title' className='section-heading'>
+                    Explore the story.
+                </h1>
+            </div>
+            <div className='flex flex-col justify-center items-center overflow-hidden'>
+                <div className='mt-32 mb-24 pl-24'>
+                    <h2 className='text-5xl lg:text-7xl font-semibold'>
+                        IPhone Forged In Titanium
+                    </h2>
+                </div>
+
+            <div className='flex-center flex-col sm:px-10'>
+                <div className='relative 5-[50vh] w-full items-center flex'>
+                    <video playsInline id='exploreVideo' className='w-full h-full object-cover object-center' 
+                    preload='none' muted autoPlay ref={videoRef}>
+                        <source src={exploreVideo} type='video/mp4' />
+                    </video>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Features
