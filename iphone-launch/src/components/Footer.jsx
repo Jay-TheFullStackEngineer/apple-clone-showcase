7b5fc52e26fc9790eaf8b4f863f8bdce2dcf5bd8
Jonathan Alex Thompson <147441174+Jay-TheFullStackEnginner@users.noreply.github.com>
2024-04-01 08:49:39 +0200

    Footer and Readme Update

--- iphone-launch/src/components/Footer.jsx
+++ iphone-launch/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         <div className='bg-neutral-700 my-5 h-[1px] w-full' />
 
         <div className='flex md:flex-row flex-col md:items-center justify-between'>
-          <p className='font-semibold text-gray text-xs'>This website is purely made as a showcase and not an offical page.</p><br/>
+          <p className='font-semibold text-gray text-xs'>This website is purely made as a showcase and is not an offical Apple page.</p><br/>
           <p className='font-semibold text-gray text-xs'>&copy;2024 Created by <a href="https://thefullstackengineer.tech">The FullStack Engineer : Jay Thompson </a></p><br/>
           <div className='flex'>
             {footerLinks.map((link, i) => (
