bb3552bce3cb705fd76944cacdbba73e58d35f58
Jonathan Alex Thompson <147441174+Jay-TheFullStackEnginner@users.noreply.github.com>
2024-04-03 19:07:49 +0200

    Restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from './pages';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import * as Sentry from '@sentry/react'
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 const App = () => {
+
+  //Sentry test method
+  //return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
-    <div className='bg-black min-h-screen w-full'>
-      <Router>
-        <Routes>
-          <Route path='/gsapto' element={<GsapTo />} />
-          <Route path='/gsapfrom' element={<GsapFrom />} />
-          <Route path='/gsapfromto' element={<GsapFromTo />} />
-          <Route path='/gsaptimeline' element={<GsapTimeline />} />
-          <Route path='/gsapstagger' element={<GsapStagger />} />
-          <Route path='/gsapscrolltrigger' element={<GsapScrollTrigger />} />
-          <Route path='/gsaptext' element={<GsapText />} />
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
